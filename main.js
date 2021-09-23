5fbfe9b920972d73bdb2ffbf8d4f2c7ff9a56e3d
Shoma-1125 <91247282+Shoma-1125@users.noreply.github.com>
2021-09-23 16:32:14 +0900

    initial commit.
    練習です。

---
+++ main.js
@@ -0,0 +1,22 @@
+$('#animation').css('visibility','hidden');
+$(window).scroll(function(){
+ var windowHeight = $(window).height(),
+     topWindow = $(window).scrollTop();
+ $('#animation').each(function(){
+  var targetPosition = $(this).offset().top;
+  if(topWindow > targetPosition - windowHeight + 100){
+   $(this).addClass("fadeInDown");
+  }
+ });
+});
+
+const CLASSNAME = "-visible";
+const TIMEOUT = 1500;
+const $target = $(".title");
+
+setInterval(() => {
+  $target.addClass(CLASSNAME);
+  setTimeout(() => {
+    $target.removeClass(CLASSNAME);
+  }, TIMEOUT);
+}, TIMEOUT * 2);
